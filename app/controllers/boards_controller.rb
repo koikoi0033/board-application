6ae7b8bdb0ef49688d521d1b7f267df1b38ecf74
YutaroKoi <koi.yutaro@gmail.com>
2018-11-12 15:19:55 +0900

    【編集画面作成に伴い、新規作成画面とのパーシャルを定義・掲示板削除機能を追加】
    ・edit.html.erbの追加と新規作成画面と共通のパーシャル_form.html.erbを作成
    ・掲示板削除機能の追加に伴い、routes.rbのルーティングのonly指定を外した
    ・controllerにdestroyアクションを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,15 +11,34 @@ class BoardsController < ApplicationController
   
   # 掲示板データ登録アクション
   def create
-    Board.create(board_params)
-
+    board = Board.create(board_params)
     # 掲示板一覧画面にリダイレクト
-    redirect_to action: :index
+    redirect_to board
   end
 
   # 掲示板詳細画面
   def show
     @board = Board.find(params[:id])
+  end
+
+  # 掲示板編集画面
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  # 掲示板編集画面の更新処理
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+    redirect_to board
+  end
+
+  # 掲示板削除機能
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+    
+    redirect_to boards_path
   end
 
   # パラメータを特定のキーにフィルタリング
